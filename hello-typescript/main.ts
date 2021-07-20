2d4ab0063024a3995108c4f94afd386f813e44c4
HebertSoares <hebert.hbs16@gmail.com>
2021-07-20 11:21:37 -0300

    att

---
+++ hello-typescript/main.ts
@@ -0,0 +1,24 @@
+var minhaVar = 'minha variavel';
+
+function minhaFunc(x, y){
+    return x + y;
+}
+//ES 6 ES 2015
+let num = 2;
+var PI = 3.14;
+
+var numero = [1, 2, 3];
+numeros.map(function(valor){
+    return valor *2;
+});
+
+numeros.map(valor => valor * 2); // ES 2015 MAIS ROBUSTO
+
+class Matematica {
+    soma (x, y){
+        return x + y;
+    }
+}
+
+var n1: any = 'dsdsds';
+n1 = 4;
